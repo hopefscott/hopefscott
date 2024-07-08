4cbebd6558bffd3be1be3f95826884cd02a172f1
hopefscott <hopescott@proton.me>
2024-07-08 06:53:44 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -19,7 +19,6 @@ document.querySelector("footer #year").textContent = footerYear.getFullYear();
 // navLinks
 let navLinks = document.querySelectorAll("nav a");
 const heroSection = document.querySelector("#hero");
-const projectsSection = document.querySelector("#projects");
 const contactSection = document.querySelector("#contact");
 let current = "current";
 
@@ -29,32 +28,25 @@ navLinks.forEach(
             navLinks.forEach((c) =>
                 c.classList[e.target == c ? "toggle" : "remove"](current)
             );
+            nav.classList.remove("menu-toggle");
 
             let containerId = e.target.textContent;
             containerId = containerId.toLowerCase();
 
             if (containerId === "home") {
+
                 document.querySelector("#hero").style.display = "flex";
                 document.querySelector("#about").style.display = "none";
-                document.querySelector("#projects").style.display = "none";
                 document.querySelector("#contact").style.display = "none";
                 document.querySelector("body").style.background = "#2d2f33";
             } else if (containerId === "about") {
                 document.querySelector("#hero").style.display = "none";
                 document.querySelector("#about").style.display = "flex";
-                document.querySelector("#projects").style.display = "none";
-                document.querySelector("#contact").style.display = "none";
-                document.querySelector("body").style.background = "#25262a";
-            } else if (containerId === "projects") {
-                document.querySelector("#hero").style.display = "none";
-                document.querySelector("#about").style.display = "none";
-                document.querySelector("#projects").style.display = "flex";
                 document.querySelector("#contact").style.display = "none";
                 document.querySelector("body").style.background = "#25262a";
             } else {
                 document.querySelector("#hero").style.display = "none";
                 document.querySelector("#about").style.display = "none";
-                document.querySelector("#projects").style.display = "none";
                 document.querySelector("#contact").style.display = "block";
                 document.querySelector("body").style.background = "#25262a";
             }
